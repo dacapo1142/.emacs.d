ff83c1b1946cbd730ebf72a93023545ababcb6d4
dacapo1142 <dacapo1142@gmail.com>
2015-12-10 02:01:00 +0800

    reset

---
+++ init.el
@@ -0,0 +1,32 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes
+   (quote
+    ("196cc00960232cfc7e74f4e95a94a5977cb16fd28ba7282195338f68c84058ec" default)))
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.org/packages/")
+     ("elpy" . "https://jorgenschaefer.github.io/packages/")
+     ("marmalade" . "http://marmalade-repo.org/packages/"))))
+ '(package-enable-at-startup nil)
+ '(package-selected-packages
+   (quote
+    (monokai-theme py-autopep8 undo-tree powerline pyvenv))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
